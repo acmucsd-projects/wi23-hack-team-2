564a3a6f67108890ed4441ccbfc356ffd095c67c
kevwu27 <115754187+kevwu27@users.noreply.github.com>
2023-03-07 21:02:27 -0800

    html code

---
+++ client/src/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="nav-bar">
+        <div className="wrapper">Build-a-Schedule
+            <div className="elmo-logo"><img src = "triton-elmo.svg" alt="Triton Elmo"/></div>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
