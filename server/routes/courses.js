efaa15e727662844069c78dd95d0ac8f7db71e31
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2023-03-09 14:41:39 -0800

    added more api routes

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -2,46 +2,31 @@ const express = require('express');
 const router = express.Router();
 const { Course, Schedule } = require('../models/model')
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-    const courses = await Course.find()
-    res.status(200).json( courses );
-});
-
-router.get('/:subj/:num', async function(req, res, next) {
+// get a specific course by name
+router.get('/courselist/:subj/:num', async function(req, res, next) {
     const courses = await Course.find({ "subj_course_id": req.params.subj + " " + req.params.num})
     res.status(200).json( courses );
 });
 
-router.get('/:subj_course_id', async function(req, res, next) {
+// alternative get a specific course by name
+router.get('/courselist/:subj_course_id', async function(req, res, next) {
     const courses = await Course.find({ "subj_course_id": req.params.subj_course_id})
     res.status(200).json( courses );
 });
-    
+   
+// get all schedules
 router.get('/schedules', async function(req, res, next) {
     const schedules = await Schedule.find()
     res.status(200).json( schedules );
 });
+   
+// find schedules for a specific person
+router.get('/schedules/:username', async function(req, res, next) {
+    const schedules = await Schedule.find({ "name": req.params.username})
+    res.status(200).json( schedules );
+});
 
-router.post('/', async (req, res) => {
-    const course = new Course({
-        subj_course_id: req.body.subj_course_id,
-        sec_code: req.body.sec_code,
-        sec_id: req.body.sec_id,
-        instructor: req.body.instructor,
-        total_seats: req.body.total_seats,
-        meetings: req.body.meetings
-
-    })
-
-    try {
-        const newCourse = await course.save()
-        res.status(201).json(newCourse)
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
-})
-
+// post a schedule for a specific person
 router.post('/schedules', async (req, res) => {
     const schedule = new Schedule({
         name: req.body.name
