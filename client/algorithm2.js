a6b991fba10f3e5fc26571169af0da570eb9b1fb
bnghiem18 <118479808+bnghiem18@users.noreply.github.com>
2023-03-11 21:30:05 -0800

    yay done we think
    kdl;jfbdfoipfi[kqpdobfsdk[dpof

--- client/algorithm2.js
+++ client/algorithm2.js
@@ -2,13 +2,35 @@
 
 test = ["CSE 11,LE,TuTh,12:00-13:20,MAND B-150", "CSE 12,LE,MWF,14:00-14:50,HSS 2150"]
 
-console.log(check(test))
+/*
+{_id: 64081caf1ddeb825edc889f9, subj_course_id: "CSE 11",sec_code:"A01",sec_id:"135615",instructor:"Cao, Yingjun",total_seats:"100",meetings:"LE,TuTh,8:00 - 9:20,SOLIS 104|FI,2023-06-15,8:00 - 10:59,SOLIS 104|DI,…"
+*/
+
+classes = [{"_id": "64081caf1ddeb825edc889f9", "subj_course_id": "CSE 11","sec_code":"A01","sec_id":"135615","instructor":"Cao, Yingjun",
+"total_seats":"100","meetings":"LE,TuTh,8:00 - 9:20,SOLIS 104|FI,2023-06-15,8:00 - 10:59,SOLIS 104|DI,…"}, 
+{"_id": "64081cae1ddeb825edc883b0",
+    "subj_course_id": "BICD 100", 
+    "sec_code": "C07", "sec_id": "165824",
+    "instructor": "Cooper, Andrew Michael",
+    "total_seats": "32",
+    "meetings": "LE,TuTh,8:00 - 9:20,SOLIS 107|FI,2023-06-15,8:00 - 10:59,SOLIS 107|DI,W,19:00 - 19:50,PCYNH 240"},
+    {
+        "_id":"64081caf1ddeb825edc886d1",
+        "subj_course_id": "CHEM 6C",
+        "sec_code": "C07",
+        "sec_id": "157585",
+        "instructor": "Brydges, Stacey",
+        "total_seats": "40",
+        "meetings": "LE,MWF,10:00 - 10:50,GH 242|FI,2023-06-12,8:00 - 10:59,GH 242|MI,2023-04-29,11:00 - 12:50,WLH 2001|MI,2023-06-03,11:00 - 12:50,WLH 2001|DI,F,11:00 - 11:50,SOLIS 111"
+      }]
+
+
 
 function check(classes) {
     var final_classes = []
 
     if (classes.length < 1 || classes.length > 4) {
-        return "Number of classes more than 4";
+        return "Need a number of classes between 1 and 4";
     }
 
     for (let i = 0; i < classes.length; i++) {
@@ -39,17 +61,17 @@ function check(classes) {
 
 //compare day
 function compare_day(classinfo1, classinfo2) {
-    list1 = classinfo1.split(",");
-    list2 = classinfo2.split(",");
+    list1 = classinfo1["meetings"].split(",");
+    list2 = classinfo2["meetings"].split(",");
 
-    days1 = list1[2];
-    days2 = list2[2];
+    days1 = list1[1];
+    days2 = list2[1];
 
-    if (list1[0] == list2[0]) {
-        return false;
+    if (days1[0] != days2[0]) {
+        return true;
     }
 
-    if (days1[0]!= days2[0]) {
+    if (days1[0] == days2[0]) {
         if (comparetime(classinfo1, classinfo2)) {
             return true;
         }
@@ -118,3 +140,5 @@ if (start2 > start1 + 0.5 || start2 < start1 - 0.9){
 
 }
   */  
+
+
